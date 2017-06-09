ef1097b3864553d878376244d4927fd7b91fb513
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-06-09 10:56:31 +0100

    Fixes startup/server tests broken due to lack of cloudwatch config

--- id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/StartupTest.scala
+++ id_minter/src/test/scala/uk/ac/wellcome/platform/idminter/StartupTest.scala
@@ -2,19 +2,26 @@ package uk.ac.wellcome.platform.idminter
 
 import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTest
-import uk.ac.wellcome.platform.idminter.utils.{IdentifiersTableInfo, MysqlLocal}
+import uk.ac.wellcome.platform.idminter.utils.{
+  IdentifiersTableInfo,
+  MysqlLocal
+}
 import uk.ac.wellcome.test.utils._
 
 class StartupTest
     extends FeatureTest
     with StartupLogbackOverride
     with SNSLocal
-      with MysqlLocal
-      with IdentifiersTableInfo
+    with MysqlLocal
+    with IdentifiersTableInfo
+    with AmazonCloudWatchFlag
     with SQSLocal {
   val server = new EmbeddedHttpServer(
     new Server(),
-    flags = snsLocalEndpointFlags ++ sqsLocalFlags ++ identifiersMySqlLocalFlags
+    flags = snsLocalEndpointFlags ++
+      sqsLocalFlags ++
+      identifiersMySqlLocalFlags ++
+      cloudWatchLocalEndpointFlag
   )
 
   test("server starts up correctly") {
